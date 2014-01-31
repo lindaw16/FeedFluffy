d49856bfead4d283e67e7ad70f1ac19a9603cdf5
Clare Liu <clareliu@mit.edu>
2014-01-30 21:43:31 -0500

    fixed pause level select

--- Projectfiles/PauseScene.mm
+++ Projectfiles/PauseScene.mm
@@ -11,14 +11,12 @@
 #import "PhysicsLayer.h"
 #import "LevelSelectLayer.h"
 #import "EasyLevelLayer.h"
+#import "MediumLevelLayer.h"
+
+int thisLevel;
 
 @implementation PauseScene
-/*+(id) scene{
-    CCScene *scene=[CCScene node];
-    PauseScene *layer = [PauseScene node];
-    [scene addChild: layer];
-    return scene;
-}*/
+
 +(id) sceneWithLevel:(int)level
 {
 	// 'scene' is an autorelease object.
@@ -31,6 +29,7 @@
 }
 
 -(id)initWithLevel: (int) level{
+    thisLevel = level;
     if( (self=[super init] )) {
         if (IsIphone5)
         {
@@ -60,36 +59,16 @@
         
         CCMenuItemImage *resume= [CCMenuItemImage itemWithNormalImage:@"resume.png"
                                                         selectedImage: @"resume2.png" target:self
-                                                  selector:@selector(resume:)];
+                                                             selector:@selector(resume:)];
         
-    
-        //CCMenuItem *Level = [CCMenuItemFont itemFromString:@"Level Select"
-                                                   //target:self selector:@selector(GoToLevels:)];
         CCMenuItemImage *level = [CCMenuItemImage itemWithNormalImage: @"level_select.png" selectedImage: @"level_select2.png" target:self
                                                             selector:@selector(GoToLevels:)];
         
-        //CCMenuItem *Quit = [CCMenuItemFont itemFromString:@"Main Menu"
-                                                   //target:self selector:@selector(GoToMainMenu:)];
+
         CCMenuItemImage *quit = [CCMenuItemImage itemWithNormalImage: @"main_menu.png" selectedImage: @"main_menu2.png" target:self selector:@selector(GoToMainMenu:)];
         
         CCMenu *menu= [CCMenu menuWithItems: resume, level, quit, nil];
         
-       /* if (IsIphone5)
-        {
-        menu.position = CGPointZero;
-        resume.position = ccp(225, 160);
-        restart.position = ccp(345, 160);
-        level.position = ccp(225, 100);
-        quit.position = ccp(345,100);
-        }
-        else {
-            menu.position = CGPointZero;
-            resume.position = ccp(185, 160);
-            restart.position = ccp(305, 160);
-            level.position = ccp(185, 100);
-            quit.position = ccp(305,100);
-        }*/
-        //[menu alignItemsVerticallyWithPadding:12.5f];
         CGSize winSize = [CCDirector sharedDirector].winSize;
         [menu alignItemsVerticallyWithPadding:12.5f];
         menu.position =  ccp(winSize.width/2, winSize.height/2);
@@ -118,27 +97,24 @@
     
     [[CCDirector sharedDirector] sendCleanupToScene];
     [[CCDirector sharedDirector] popScene];
+    
+    if (thisLevel < 13){
     [[CCDirector sharedDirector] replaceScene:[CCTransitionFade
                                                transitionWithDuration:1
                                                scene:[EasyLevelLayer node]]
      ];
-}
-
-
--(void) Restart: (CCMenuItem*) sender {
-    
-    [[CCDirector sharedDirector] sendCleanupToScene];
-    [[CCDirector sharedDirector] popScene];
-    int level = sender.tag;
-    //[[CCDirector sharedDirector] replaceScene: (CCScene*)[PhysicsLayer sceneWithLevel:level]];
-    [[CCDirector sharedDirector] replaceScene: [CCTransitionFade transitionWithDuration:1 scene:[PhysicsLayer sceneWithLevel:level]]];
-
+    }
+    else {
+        [[CCDirector sharedDirector] replaceScene:[CCTransitionFade
+                                                   transitionWithDuration:1
+                                                   scene:[MediumLevelLayer node]]
+         ];
+    }
 }
 
 -(void) onExit {
     //unschedule selectors to get dealloc to fire off
     [self unscheduleAllSelectors];
-    //remove all textures to free up additional memory. Textures get retained even if the sprite gets released and it doesn't show as a leak. This was my big memory saver
     [[CCTextureCache sharedTextureCache] removeAllTextures];
     [super onExit];
 }
